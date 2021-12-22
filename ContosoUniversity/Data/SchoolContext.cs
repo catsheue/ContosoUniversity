ff7a2cb24941963b4c6eeca5e66ebbbd125b33b2
Luna Goodnight <luna.lee@nextfortune.tw>
2021-12-22 11:14:33 +0800

    [Add] ensureCreated

--- ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -14,7 +14,16 @@ namespace ContosoUniversity.Data
             : base(options)
         {
         }
+        
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
 
-        public DbSet<ContosoUniversity.Models.Student> Student { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
     }
 }
